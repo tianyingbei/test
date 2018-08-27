5285f7ac1ce695e48a6dfd2a61fb59b9f2d5e4f7
leoualy <liuyuanaaron@icloud.com>
2018-08-27 17:35:53 +0800

    沙发 暗示发

--- src/main/java/test/hello.java
+++ src/main/java/test/hello.java
@@ -2,6 +2,6 @@ package test;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("222222222222222222222222");
+        System.out.println("222222222222222222222224444");
     }
 }
